2ffb3e92f7eb2fd7c63733f509e648d84ba3aa07
Pedro <Pedrogregolini@gmail.com>
2025-03-28 11:59:09 -0300

    Subindo codigo

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+// Declaração e variaveis
+
+var nome = "Fiap";
+console.log(nome)
+
+let idade = 19;
+console.log(idade)
+
+const sobrenome = "Fiapinho"
+console.log(sobrenome)
+
+// Undefined
+let exemplo;
+console.log(exemplo)
+
+//nula
+let exemplo1 = null
+console.log(exemplo1)
+
+//Consultar os tipos de variaveis
+let ex1 = "Fiap";
+console.log(typeof ex1)
+
+let ex2 = 2;
+console.log(typeof ex2)
+
+let ex3 = true
+console.log(typeof ex3)
+
+let ex4 = {}
+console.log(typeof ex4)
+
+let ex5 = []
+console.log(typeof ex5)
